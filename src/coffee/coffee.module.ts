80051ffdc2a4cec5afb099312b17b7f896a4dc1f
Vladislav Kuz'menko <70003051+Heckatokronos@users.noreply.github.com>
2023-12-05 15:42:03 +1100

    Flavor feature update

--- src/coffee/coffee.module.ts
+++ src/coffee/coffee.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeResolver } from './coffee.resolver';
 import { CoffeeService } from './coffee.service';
 import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coffee])],
+  imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   providers: [CoffeeResolver, CoffeeService],
 })
 export class CoffeeModule {}
